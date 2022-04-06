3e7171b308864dae9e1661b749eb31dc8d24e9b5
1947591 <1947591@01-19956.pusd.dom>
2022-04-06 10:17:51 -0700

    Addition of tiles and the Arena class

---
+++ src/Arena.java
@@ -0,0 +1,12 @@
+
+
+public class Arena {
+	
+	protected Tile[][] arena;
+	
+	
+	
+	
+	
+	
+}
--- src/Tile.java
+++ src/Tile.java
@@ -1,73 +1,29 @@
 public class Tile {
 
 	/*
-	 * tile0 = Ground tile tile1 = Hole tile tile2 = Wall tile tile3 = Fragile wall
-	 * tile tile4 = Broken tile tile5 = Slow ground tile tile6 = Fast ground tile
-	 * tile7 = Ice tile(???)
+	 * tile0 = Ground tile
+	 * tile1 = Hole tile
+	 * tile2 = Wall tile
+	 * tile3 = Fragile wall tile
+	 * tile4 = Broken tile
+	 * tile5 = Slow ground tile
+	 * tile6 = Fast ground tile
+	 * tile7 = Powerup tile
+	 * tile8 = Ice tile(???)
 	 */
-
+	
+	int type;
 	protected boolean floor, clear, frail;
 	protected double speed;
 	private String txtr;
 
 	public Tile() {
+		type = 0;
 		floor = true;
 		clear = true;
 		frail = false;
 		speed = 1;
 		txtr = "tile0";
-	}
-
-	public Tile(int type) {
-		if (type == 0) {
-			floor = true;
-			clear = true;
-			frail = false;
-			speed = 0.6;
-			txtr = "tile0";
-		}
-		if (type == 1) {
-			floor = true;
-			clear = true;
-			frail = false;
-			speed = 0.6;
-			txtr = "tile1";
-		}
-		if (type == 2) {
-			floor = true;
-			clear = true;
-			frail = false;
-			speed = 0.6;
-			txtr = "tile2";
-		}
-		if (type == 3) {
-			floor = true; 
-			clear = true;
-			frail = false;
-			speed = 0.6;
-			txtr = "tile3";
-		}
-		if (type == 4) {
-			floor = true;
-			clear = true;
-			frail = false;
-			speed = 0.6;
-			txtr = "tile4";
-		}
-		if (type == 5) {
-			floor = true;
-			clear = true;
-			frail = false;
-			speed = 0.6;
-			txtr = "tile5";
-		}
-		if (type == 6) {
-			floor = true;
-			clear = true;
-			frail = false;
-			speed = 0.6;
-			txtr = "tile6";
-		}
 	}
 
 	public void frag() {
