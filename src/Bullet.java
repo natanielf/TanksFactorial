fff5502630ee5fd56702ce9b87386a62705f2d6c
1947591 <1947591@01-19956.pusd.dom>
2022-04-08 08:49:02 -0700

    added paint method to bullet class

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,12 +1,29 @@
+import java.awt.Graphics;
+import java.awt.Color;
+
 public class Bullet {
 	
-	protected int x, y, vX, vY;
+	protected int x, y, vX, vY, size;
+	private Color color;
 
 	public Bullet(int x, int y, int vX, int vY) {
 		this.x = x;
 		this.y = y;
 		this.vX = vX;
 		this.vY = vY;
+		size = 10;
+		color = new Color(25, 90, 150);
+	}
+	
+	public void paint(Graphics g) {
+		Color oldColor = g.getColor();
+		g.setColor(color);
+		x += vX;
+		y += vY;
+		g.fillOval(x, y, 10, 10);
+		
+		//TODO: 
+		
 	}
 	
 	public void bounce(int d) {
@@ -22,5 +39,8 @@ public class Bullet {
 			vX = Math.abs(vX);
 		}
 	}
+	
+	
+	
 
 }
