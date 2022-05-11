0d6a73030c69a99362e3dbf1a7ae54dda7c49731
natanielf <natanielfarzan@gmail.com>
2022-05-10 17:38:22 -0700

    Start project refactor
    - Begin transitioning to the Processing library

--- src/Arena.java
+++ src/Arena.java
@@ -1,63 +1,35 @@
-import java.awt.Graphics;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Scanner;
 
+import processing.core.PApplet;
+
 public class Arena {
-	// mult(iplier) can be used to increase the size of the game screen
-	private int mult;
+
+	private PApplet app;
 	protected Tile[][] grid;
 	protected int width, height, rows, cols, tileSize, tileSpacer;
 
-	public Arena() {
+	public Arena(PApplet app, int fW, int fH) {
+		this.app = app;
 		this.rows = 18;
 		this.cols = 32;
-		createArena();
-		this.tileSize = 30;
-		this.tileSpacer = 1;
-		mult = 1;
-	}
-
-	public Arena(File f) {
-		this.rows = 18;
-		this.cols = 32;
-		this.tileSize = 30;
-		this.tileSpacer = 1;
-		this.mult = 1;
-		createArena();
-		textFileInput(f);
-	}
-
-	public Arena(int fW, int fH) {
-		this.rows = 18;
-		this.cols = 32;
-		createArena();
 		this.width = fW;
 		this.height = fH;
 		this.tileSpacer = 1;
 		this.tileSize = (int) 3 * Math.min(width / (cols + (cols * tileSpacer)), height / (rows + (rows * tileSpacer)));
-		mult = 1;
+		createArena();
 	}
 
-	public Arena(int fW, int fH, File f) {
-		this(fW, fH);
+	public Arena(PApplet app, int fW, int fH, File f) {
+		this(app, fW, fH);
 		textFileInput(f);
 	}
 
-	public Arena(int m) {
-		this.rows = 18;
-		this.cols = 32;
-		createArena();
-		this.tileSpacer = 3;
-		this.tileSize = (int) 3 * Math.min(width / (cols + (cols * tileSpacer)), height / (rows + (rows * tileSpacer)));
-		mult = m;
-	}
-
-	public void paint(Graphics g) {
+	public void paint() {
 		for (int r = 0; r < rows; r++) {
 			for (int c = 0; c < cols; c++) {
-				grid[r][c].paint(g, c * tileSize + (tileSpacer * c), r * tileSize + (tileSpacer * r), tileSize);
+				grid[r][c].paint();
 			}
 		}
 	}
@@ -66,41 +38,36 @@ public class Arena {
 		this.grid = new Tile[rows][cols];
 		for (int r = 0; r < rows; r++) {
 			for (int c = 0; c < cols; c++) {
-				grid[r][c] = new Tile();
+				grid[r][c] = new Tile(app, c * (tileSize + tileSpacer), r * (tileSize + tileSpacer), tileSize, 0);
 			}
 		}
 	}
 
-	public void updateSize(int fW, int fH) {
-		this.width = fW;
-		this.height = fH;
-		this.tileSize = (int) 3 * Math.min(width / (cols + (cols * tileSpacer)), height / (rows + (rows * tileSpacer)));
-	}
-
 	public void textFileInput(File f) {
 		try {
-			Scanner s = new Scanner(f);
-			int r = 0;
-			while (s.hasNextLine() && r < grid.length) {
-				String line = s.nextLine();
-				char[] lineArray = line.toCharArray();
-				for (int i = 0; i < lineArray.length; i++) {
-					char c = lineArray[i];
-					int type = 0;
-					switch (c) {
-					case '_':
-						type = 0;
-						break;
-					case 'X':
-						type = 1;
-						break;
-					default:
-						type = -1;
-						break;
+			try (Scanner s = new Scanner(f)) {
+				int r = 0;
+				while (s.hasNextLine() && r < grid.length) {
+					String line = s.nextLine();
+					char[] lineArray = line.toCharArray();
+					for (int i = 0; i < lineArray.length; i++) {
+						char c = lineArray[i];
+						int type = 0;
+						switch (c) {
+							case '_':
+								type = 0;
+								break;
+							case 'X':
+								type = 1;
+								break;
+							default:
+								type = -1;
+								break;
+						}
+						grid[r][i].setType(type);
 					}
-					grid[r][i].setType(type);
+					r++;
 				}
-				r++;
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
--- src/Game.java
+++ src/Game.java
@@ -1,32 +1,188 @@
+import java.io.File;
+
 import processing.core.PApplet;
+import processing.core.PImage;
+import processing.core.PSurface;
 
 public class Game extends PApplet {
 
+	private Arena arena;
+	private PlayerTank player;
+	// private PSurface surface;
+
 	public static void main(String[] args) {
 		String[] processingArgs = { "Tanks!" };
-		Game test = new Game();
-		PApplet.runSketch(processingArgs, test);
+		Game g = new Game();
+		PApplet.runSketch(processingArgs, g);
+	}
+
+	public Game() {
+		this.arena = new Arena(this, displayWidth, displayHeight, new File("./maps/test.txt"));
+		createSurface();
 	}
 
 	@Override
 	public void settings() {
-		// size(displayWidth, displayHeight);
-		fullScreen();
+		size(1280, 720);
 	}
 
 	@Override
 	public void setup() {
+		surface.setTitle("Tanks!");
+		surface.setResizable(true);
 		rectMode(CENTER);
 		noStroke();
-		fill(0, 102, 153, 204);
+		fill(10, 10, 20, 20);
 	}
 
 	public void draw() {
 		cursor(CROSS);
 		background(200);
 		rect(50, 50, 40, 40);
-		stroke(0);
+		stroke(0, 0, 0);
 		line(60, 60, mouseX, mouseY);
+		arena.paint();
+	}
+
+	private void createSurface() {
+		this.surface = new PSurface() {
+
+			@Override
+			public Object getNative() {
+				return null;
+			}
+
+			@Override
+			public void hideCursor() {
+
+			}
+
+			@Override
+			public void initFrame(PApplet arg0) {
+
+			}
+
+			@Override
+			public void initOffscreen(PApplet arg0) {
+
+			}
+
+			@Override
+			public boolean isStopped() {
+				return false;
+			}
+
+			@Override
+			public PImage loadImage(String arg0, Object... arg1) {
+				return null;
+			}
+
+			@Override
+			public boolean openLink(String arg0) {
+				return false;
+			}
+
+			@Override
+			public void pauseThread() {
+
+			}
+
+			@Override
+			public void placePresent(int arg0) {
+
+			}
+
+			@Override
+			public void placeWindow(int[] arg0, int[] arg1) {
+
+			}
+
+			@Override
+			public void resumeThread() {
+
+			}
+
+			@Override
+			public void selectFolder(String arg0, String arg1, File arg2, Object arg3) {
+
+			}
+
+			@Override
+			public void selectInput(String arg0, String arg1, File arg2, Object arg3) {
+
+			}
+
+			@Override
+			public void selectOutput(String arg0, String arg1, File arg2, Object arg3) {
+
+			}
+
+			@Override
+			public void setAlwaysOnTop(boolean arg0) {
+
+			}
+
+			@Override
+			public void setCursor(int arg0) {
+
+			}
+
+			@Override
+			public void setCursor(PImage arg0, int arg1, int arg2) {
+
+			}
+
+			@Override
+			public void setFrameRate(float arg0) {
+
+			}
+
+			@Override
+			public void setIcon(PImage arg0) {
+
+			}
+
+			@Override
+			public void setLocation(int arg0, int arg1) {
+
+			}
+
+			@Override
+			public void setResizable(boolean arg0) {
+
+			}
+
+			@Override
+			public void setSize(int arg0, int arg1) {
+
+			}
+
+			@Override
+			public void setTitle(String arg0) {
+
+			}
+
+			@Override
+			public void setVisible(boolean arg0) {
+
+			}
+
+			@Override
+			public void showCursor() {
+
+			}
+
+			@Override
+			public void startThread() {
+
+			}
+
+			@Override
+			public boolean stopThread() {
+				return false;
+			}
+
+		};
 	}
 
 }
