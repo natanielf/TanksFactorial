ac02a932947b0ab7774610f24b25808c5a299277
1947591 <1947591@01-19956.pusd.dom>
2022-05-23 09:19:28 -0700

    added collision response to tank

--- src/Tank.java
+++ src/Tank.java
@@ -10,7 +10,9 @@ public class Tank {
 	protected int ammo, maxAmmo, speed, size;
 	protected ArrayList<Bullet> bullets;
 
-	public Tank(PApplet app, int x, int y, int s) {
+	private Arena map;
+
+	public Tank(PApplet app, int x, int y, int s, Arena map) {
 		this.app = app;
 		this.location = new PVector(x, y);
 		this.velocity = new PVector(0, 0);
@@ -19,6 +21,7 @@ public class Tank {
 		speed = 6;
 		size = s;
 		bullets = new ArrayList<>();
+		this.map = map;
 	}
 
 	public void moveEast() {
@@ -61,12 +64,20 @@ public class Tank {
 
 	public void update() {
 		if (velocity.x != 0 && velocity.y != 0) {
-			location.x += velocity.x * 0.75;
-			location.y += velocity.y * 0.75;
+			location.x += velocity.x / Math.sqrt(2);
+			location.y += velocity.y / Math.sqrt(2);
 		} else {
 			location.x += velocity.x;
 			location.y += velocity.y;
 		}
+		
+		if(map.collide(location.x, location.y) {
+			stopX();
+			stopY();
+			System.out.println("I Better Call Saul!");
+		}
+		
+		
 	}
 
 	public void shoot(int mX, int mY) {
