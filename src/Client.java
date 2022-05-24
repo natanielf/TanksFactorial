996f7148a04ef96e07b23ded3af34bdac2cbe434
natanielf <natanielfarzan@gmail.com>
2022-05-24 16:29:22 -0700

    Clean up error handling in `Client` class

--- src/Client.java
+++ src/Client.java
@@ -11,30 +11,38 @@ public class Client {
 	private DataOutputStream out;
 	private String address;
 	private int port;
+	private boolean connected;
 
 	public Client(String address, int port) throws IOException {
 		this.address = address;
 		this.port = port;
+		this.connected = false;
 
 		try {
+			System.out.println("Connecting to " + address + ":" + port + "...");
 			socket = new Socket(address, port);
 			in = new DataInputStream(System.in);
 			out = new DataOutputStream(socket.getOutputStream());
+			connected = true;
 		} catch (UnknownHostException e) {
-			e.printStackTrace();
+			System.err.println(e);
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.err.println(e);
 		}
 
 		// TODO: Event loop logic for client
 
-		try {
-			in.close();
-			out.close();
-			socket.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+		if (connected) {
+			try {
+				in.close();
+				out.close();
+				socket.close();
+				connected = false;
+			} catch (IOException e) {
+				System.err.println(e);
+			}
 		}
+
 	}
 
 	public static void main(String[] args) throws IOException {
