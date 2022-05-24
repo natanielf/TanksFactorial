6ff3f688e8c93f5b26d7e60ce28bcafed3977c18
natanielf <natanielfarzan@gmail.com>
2022-05-23 19:05:34 -0700

    Create `Server` class for websocket connections
    - This commit as well as the previous one are based this example: https://codezup.com/socket-programming-client-and-server-in-java-example/

--- src/Client.java
+++ src/Client.java
@@ -6,38 +6,39 @@ import java.net.UnknownHostException;
 
 public class Client {
 
-    private Socket socket;
-    private DataInputStream in;
-    private DataOutputStream out;
-    private String address;
-    private int port;
+	private Socket socket;
+	private DataInputStream in;
+	private DataOutputStream out;
+	private String address;
+	private int port;
 
-    public Client(String address, int port) throws IOException {
-        this.address = address;
-        this.port = port;
+	public Client(String address, int port) throws IOException {
+		this.address = address;
+		this.port = port;
 
-        try {
-            socket = new Socket(address, port);
-            in = new DataInputStream(System.in);
-            out = new DataOutputStream(socket.getOutputStream());
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		try {
+			socket = new Socket(address, port);
+			in = new DataInputStream(System.in);
+			out = new DataOutputStream(socket.getOutputStream());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        try {
-            in.close();
-            out.close();
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		// TODO: Event loop logic for client
 
-    }
+		try {
+			in.close();
+			out.close();
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
-        new Client("127.0.0.1", 80);
-    }
+	public static void main(String[] args) throws IOException {
+		new Client("127.0.0.1", 80);
+	}
 
 }
